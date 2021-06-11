9516d39ebb72674fe1c3fd40da27e5ec285caf81
emi <emiliorizzo@gmail.com>
2021-06-11 23:18:12 +0000

    Update PageNotFound

--- dapps/images-client/src/components/PageNotFound.vue
+++ dapps/images-client/src/components/PageNotFound.vue
@@ -1,8 +1,23 @@
 <template lang="pug">
-h1 404
+  .not-found
+    h1 404
+    h2 Page not found
 </template>
 <script>
 export default {
   name: 'page-not-found'
 }
 </script>
+<style lang="stylus">
+@import '../style/mixins.styl'
+@import '../style/variables.styl'
+  .not-found
+    flex-centered()
+    flex-flow column wrap
+    h1
+      margin 0
+      font-size 10em
+      text-shadow textShadow
+    h2
+      font-size 2.5em
+</style>
