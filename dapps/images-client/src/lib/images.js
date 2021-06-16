120a185dbfbd61834952a4451281833628503956
emi <emiliorizzo@gmail.com>
2021-06-16 22:18:45 +0000

    Update images

--- dapps/images-client/src/lib/images.js
+++ dapps/images-client/src/lib/images.js
@@ -33,7 +33,35 @@ export default {
   },
   ethRopsten: {},
   rskMainnet: {
-    '0xa80d1090d95787eb445b8b7c3c988c41357c0604': portrait
+    '0xa80d1090d95787eb445b8b7c3c988c41357c0604': portrait,
+    '0x08F18ED92929FaA1f115C55a5e5Dde28bA02F52e': {
+      title: 'Butterfly',
+      width: 598,
+      height: 526,
+      extension: 'jpg',
+      description
+    },
+    '0x5617aeb81204906e3b77d70123e9923e062abc2d': {
+      title: 'Woman',
+      width: 900,
+      height: 1217,
+      extension: 'jpg',
+      description
+    },
+    '0x75fd4a189b3b4d1e44dd1919de3fcb1d3588f833': {
+      title: 'Eiffel',
+      width: 800,
+      height: 1138,
+      extension: 'jpg',
+      description
+    },
+    '0x9d576DbD4b6D333d9F496b639B4687Cb41ff3e46': {
+      title: 'Squirrel',
+      width: 1000,
+      height: 1417,
+      extension: 'jpg',
+      description
+    }
   },
   rskTestnet
 }
